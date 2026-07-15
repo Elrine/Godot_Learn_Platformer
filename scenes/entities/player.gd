ce42ec9184c31f8ce8e5394e340e3b73520a12cd
Nathan Harlé <nathan.h@azursoft.com>
2026-07-15 18:04:56 +0200

    Create signal

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -3,19 +3,19 @@ extends CharacterBody2D
 
 var direction_x : float
 var speed := 50
-var can_shoot : bool = true
 
 @export var jump_strength := 200
 @export var gravity := 1000
+
+signal shoot(pos: Vector2, dir: Vector2)
 
 
 func get_input() -> void:
 	direction_x = Input.get_axis("left", "right")
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = -jump_strength
-	if Input.is_action_just_pressed("shoot") and can_shoot:
-		print("shoot")
-		can_shoot = false
+	if Input.is_action_just_pressed("shoot") and $Shoot_cooldown.time_left == 0:
+		shoot.emit(position, get_local_mouse_position().normalized())
 		$Shoot_cooldown.start(0.5)
 
 
@@ -28,7 +28,3 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction_x * speed
 	apply_gravity(delta)
 	move_and_slide()
-
-
-func _on_shoot_cooldown_timeout() -> void:
-	can_shoot = true
